6d091122cc1dadd7f5f54e170ba9be6586cbe3b3
Allan Degnan <allan@adegnan.net>
2016-01-08 18:04:38 +0000

    added scheme so we dont redir to http

--- entry-point.sh
+++ entry-point.sh
@@ -50,11 +50,13 @@ http {
  
         location / {
             proxy_pass https://backend;
+            proxy_ssl on;
             proxy_ssl_verify off;
             proxy_redirect     off;
-            proxy_set_header   Host \$host;
+            proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;     
+            proxy_set_header X_FORWARDED_PROTO \$scheme;
         }
     }
 }
