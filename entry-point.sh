380d6b748d8a0ff3e76e212c0f52e32bd47e658d
Allan Degnan <allan@adegnan.net>
2016-01-08 17:26:41 +0000

    attempting to maintain the url

--- entry-point.sh
+++ entry-point.sh
@@ -48,10 +48,13 @@ http {
         listen       [::]:${PORT:-8000} default_server;
         server_name  _;
 
-        location / {
-            proxy_pass https://backend;
-            proxy_ssl_verify off;
-        }
+        proxy_pass backend;
+        proxy_ssl  on;
+        proxy_ssl_verify off;
+        proxy_redirect     off;
+        proxy_set_header   Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;     
     }
 }
 EOF
